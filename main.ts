2b296a55ab9b3c631f26665ad5e2c316cf3749db
Andrew Steinheiser <me@iamandrew.io>
2021-09-17 00:34:17 -0700

    use dotenv and basic AWS EC2 example

--- main.ts
+++ main.ts
@@ -1,31 +1,28 @@
+import * as dotenv from 'dotenv'
 import { Construct } from 'constructs'
-import { App, TerraformStack } from 'cdktf'
-import { Container, Image, DockerProvider } from './.gen/providers/docker'
+import { App, TerraformStack, TerraformOutput } from 'cdktf'
+import { AwsProvider, Instance } from './.gen/providers/aws'
+
+dotenv.config()
+const AWS_REGION = process.env.AWS_REGION || 'us-west-1';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name)
 
-    new DockerProvider(this, 'default', {})
+    new AwsProvider(this, 'aws', { region: AWS_REGION })
 
-    const dockerImage = new Image(this, 'nginxImage', {
-      name: 'nginx:latest',
-      keepLocally: false,
+    const instance = new Instance(this, 'compute', {
+      ami: 'ami-01456a894f71116f2',
+      instanceType: 't2.micro',
     })
 
-    new Container(this, 'nginxContainer', {
-      image: dockerImage.latest,
-      name: 'tutorial',
-      ports: [
-        {
-          internal: 80,
-          external: 8000,
-        },
-      ],
+    new TerraformOutput(this, 'public_ip', {
+      value: instance.publicIp,
     })
   }
 }
 
 const app = new App()
-new MyStack(app, 'typescript-docker')
+new MyStack(app, 'typescript-aws')
 app.synth()
